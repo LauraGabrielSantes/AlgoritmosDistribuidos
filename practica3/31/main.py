1a7d3e5fd165e2554ab02c65af1653e285b28db7
Laura Gabriel <75498044+LauraGabrielSantes@users.noreply.github.com>
2026-06-02 07:16:59 -0600

    LRC con casos en mensajes

---
+++ practica3/31/main.py
@@ -0,0 +1,31 @@
+# "main()"
+# ----------------------------------------------------------------------------------------
+# construye una instancia de la clase Simulation recibiendo como parametros el nombre del 
+# archivo que codifica la lista de adyacencias de la grafica y el tiempo max. de simulacion
+import sys
+import time
+from event import Event
+from simulation import Simulation
+from algoritmoLCR import AlgoritmoLCR
+
+class main():
+    if len(sys.argv) != 2:
+        print ("Por favor proporcione el nombre de la grafica de comunicaciones")
+        raise SystemExit(1)
+    
+    inicio = time.time()
+    experiment = Simulation(sys.argv[1], 20)  
+    m = AlgoritmoLCR()
+    # asocia un pareja proceso/modelo con cada nodo de la grafica
+    for i in range(1,len(experiment.graph)+1):
+        experiment.setModel(m, i)
+        m = AlgoritmoLCR()
+
+    # inserta un evento semilla en la agenda y arranca
+    inicia=("INICIA",None)
+    seed = Event(inicia, 0.0, 1, 1)
+    experiment.init(seed)
+    experiment.run()
+    fin = time.time()
+    print("Se han generado ", AlgoritmoLCR.contadorMensajes," mensajes en total \n")
+    print("Tiempo de ejecución:", fin-inicio, "segundos \n")
---
+++ practica3/32a/algoritmoLCR.py
@@ -0,0 +1,64 @@
+#Implementa la simulacion del Algoritmo de Le Lann, Chang y Roberts 
+# con el mejor caso en mensajes
+
+import sys
+from event import Event
+from model import Model
+from process import Process
+from simulator import Simulator
+from simulation import Simulation
+
+
+class AlgoritmoLCR(Model):
+  # Esta clase desciende de la clase Model e implementa los metodos 
+  # "init()" y "receive()", que en la clase madre se definen como abstractos
+  contadorMensajes=0
+  def init(self):
+    # Aqui se definen e inicializan los atributos particulares del algoritmo
+    print ("Inicio funciones", self.id)
+    self.sucesor = self.neighbors[0]
+    print ("Mi vecino es:", self.sucesor)
+    #self.estado="dormido"
+    self.lider=0
+    self.CandidaturaLanzada=False
+
+  def receive(self, event):
+    # Aqui se definen las acciones concretas que deben ejecutarse cuando se
+    # recibe un evento
+    if event.getName()[0] == "INICIA":
+       print ("[", self.id, "]: recibi INICIA en t=",self.clock," \n")
+       self.CandidaturaLanzada=True
+       self.enviaCandidatura(self.id)
+    elif  event.getName()[0] == "CANDIDATURA" :
+       AlgoritmoLCR.contadorMensajes += 1
+       if (self.id < event.getName()[1]):
+          print("[", self.id, "]:He recibido la candidatura de ", event.getName()[1], " en t= ", self.clock,"\n" )
+          print("[", self.id, "]:No envío mi candidatura \n" )
+          self.enviaCandidatura(event.getName()[1])
+       elif (self.id > event.getName()[1]  and self.CandidaturaLanzada==False):
+          print("[", self.id, "]:He recibido la candidatura de ", event.getName()[1], " en t= ", self.clock,"\n" )
+          self.CandidaturaLanzada==True
+          self.enviaCandidatura(self.id)
+       else:
+          print("[", self.id, "]:He recibido mi candidatura en t= ", self.clock,"\n" )
+          self.enviaElecto(self.id)
+    elif event.getName()[0] == "ELECTO" :
+       AlgoritmoLCR.contadorMensajes +=1
+       if(self.id != event.getName()[1]):
+          print("[",self.id,"]: ", event.getName()[1] ," ha ganado la elección en", self.clock," \n")
+          self.enviaElecto(event.getName()[1])
+       else:
+          print("[", self.id,"]: He sido electo en", self.clock,"\n")
+          
+  
+  def enviaCandidatura(self, id):
+     candidatura=("CANDIDATURA", id)
+     newevent = Event(candidatura, self.clock + 1.0, self.sucesor, self.id)
+     self.transmit(newevent)
+  
+  def enviaElecto(self, id):
+     self.lider=id
+     self.CandidaturaLanzada=False
+     electo=("ELECTO", id)
+     newevent = Event(electo, self.clock + 1.0, self.sucesor, self.id)
+     self.transmit(newevent)
